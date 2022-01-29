4d8c7acde87273a2a4f50e4499e19a8d47c8a3e4
Marcos Á. Medina <marcmedi1@hotmail.com>
2022-01-29 00:20:31 -0300

    sass and wip Component  added

---
+++ mkmcomponents/src/components/mkm-wip/mkm-wip.tsx
@@ -0,0 +1,18 @@
+import { Component, /*Prop,*/ h} from '@stencil/core';
+@Component({
+	tag: "mkm-wip",
+	styleUrl: "mkm-wip.scss",
+	// shadow: true
+})
+
+export class MkmWip{
+	// @Prop() color: string;
+
+	render(){
+		return(
+			<aside>
+				<span>Work in Progress</span>
+			</aside>
+		)
+	}
+}
---
+++ mkmcomponents/src/components/mkm-wip/readme.md
@@ -0,0 +1,10 @@
+# mkm-wip
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
